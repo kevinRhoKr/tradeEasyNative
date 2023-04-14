50bc1bfa4d6e7d54d51eebbc8bd611c2aad299ed
kevinRho <kevinrho51@gmail.com>
2023-04-14 08:33:10 -0400

    correction to chatpage

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -5,12 +5,6 @@ export function ChatPage() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Chats!</Text>
-      <Image
-        style={{ width: "100%", height: "50%" }}
-        source={{
-          uri: "https://tradeeasy.s3.amazonaws.com/1681361960542-C446006A-553C-45E6-8918-ECA68B8EF62D.jpg",
-        }}
-      ></Image>
     </View>
   );
 }
